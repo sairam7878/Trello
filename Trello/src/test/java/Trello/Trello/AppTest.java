4e75dd3b1dce8427bd615ac9d84f49dac39ccc56
santh <santh@Santhosh>
2023-05-16 19:53:06 +0530

    FIRST commit

---
+++ Trello/src/test/java/Trello/Trello/AppTest.java
@@ -0,0 +1,13 @@
+package Trello.Trello;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
